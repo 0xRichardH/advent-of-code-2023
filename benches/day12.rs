75f78cf9ea1dc244d8e81cdbd37f7964ac616d04
Richard Hao <richard@0xdev.dev>
2023-12-13 11:09:47 +0800

    chore: update benchmarks

--- benches/day12.rs
+++ benches/day12.rs
@@ -6,14 +6,12 @@ fn main() {
     divan::main();
 }
 
-// skip because it's too slow
-// which is using brutal force
-// #[divan::bench]
-// fn part1() {
-//     part1::process_data(black_box(include_str!("../inputs/day12-input.txt"))).unwrap();
-// }
+#[divan::bench]
+fn part1() {
+    part1::process_data(black_box(include_str!("../inputs/day12-input.txt"))).unwrap();
+}
 
-// #[divan::bench]
-// fn part2() {
-//     part2::process_data(black_box(include_str!("../inputs/day12-input.txt"))).unwrap();
-// }
+#[divan::bench]
+fn part2() {
+    part2::process_data(black_box(include_str!("../inputs/day12-input.txt"))).unwrap();
+}
