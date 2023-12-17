7b953a38dc87abb8014eea2b251a551eb681a3f2
Richard Hao <richard@0xdev.dev>
2023-12-17 21:52:24 +0800

    feat(day13): try to Dijkstra's algorithm to resolve the task1

---
+++ src/day17/part1.rs
@@ -0,0 +1,138 @@
+use std::collections::BinaryHeap;
+
+const MAX_STRAIGHT_STEPS: usize = 3;
+const UP: (isize, isize) = (-1, 0);
+const DOWN: (isize, isize) = (1, 0);
+const LEFT: (isize, isize) = (0, -1);
+const RIGHT: (isize, isize) = (0, 1);
+const DIRECTIONS: [(isize, isize); 4] = [UP, DOWN, LEFT, RIGHT];
+
+#[derive(Clone, Copy, Eq, PartialEq)]
+struct State {
+    heat: usize,
+    position: (usize, usize),
+}
+
+impl Ord for State {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        other
+            .heat
+            .cmp(&self.heat)
+            .then_with(|| self.position.cmp(&other.position))
+    }
+}
+
+impl PartialOrd for State {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+pub fn process_data(input: &str) -> usize {
+    let grid = input
+        .lines()
+        .map(|l| l.chars().flat_map(|c| c.to_digit(10)).collect())
+        .collect::<Vec<Vec<u32>>>();
+
+    let starting_point = (0, 0); // top-left
+    let ending_point = (grid.len() - 1, grid[0].len() - 1); // bottom-right
+                                                            //
+    find_shortest_path(&grid, starting_point, ending_point)
+}
+
+fn find_shortest_path(
+    grid: &[Vec<u32>],
+    starting_point: (usize, usize),
+    ending_point: (usize, usize),
+) -> usize {
+    let mut distance = vec![vec![usize::MAX; grid[0].len()]; grid.len()];
+    let mut pq = BinaryHeap::<State>::new();
+    pq.push(State {
+        heat: 0,
+        position: starting_point,
+    });
+    distance[starting_point.0][starting_point.1] = 0;
+
+    let grid_x_len = grid.len() as isize;
+    let grid_y_len = grid[0].len() as isize;
+
+    let mut prev_direction = RIGHT;
+    let mut prev_direction_counter = 1;
+
+    while let Some(State { heat, position }) = pq.pop() {
+        // reach the ending_point
+        if position == ending_point {
+            return heat;
+        }
+
+        // skip the most heaty point
+        if heat > distance[position.0][position.1] {
+            continue;
+        }
+
+        // find the next point by direction
+        for (dx, dy) in DIRECTIONS {
+            // can't reverse direction
+            match prev_direction {
+                UP if (dx, dy) == DOWN => continue,
+                DOWN if (dx, dy) == UP => continue,
+                LEFT if (dx, dy) == RIGHT => continue,
+                RIGHT if (dx, dy) == LEFT => continue,
+                _ => (),
+            }
+
+            let (x, y) = (position.0 as isize + dx, position.1 as isize + dy);
+            // check the bound
+            if x < 0 || x >= grid_x_len || y < 0 || y >= grid_y_len {
+                continue;
+            }
+
+            let point_heat = grid[x as usize][y as usize] as usize;
+            let next = State {
+                heat: heat + point_heat,
+                position: (x as usize, y as usize),
+            };
+            if next.heat < distance[next.position.0][next.position.1] {
+                // we can only go straight for MAX_STRAIGHT_STEPS
+                if prev_direction == (dx, dy) {
+                    if prev_direction_counter > MAX_STRAIGHT_STEPS {
+                        continue;
+                    }
+                    prev_direction_counter += 1;
+                } else {
+                    prev_direction = (dx, dy);
+                    prev_direction_counter = 1;
+                }
+
+                // enqueue the least heat point
+                pq.push(next);
+                distance[next.position.0][next.position.1] = next.heat;
+            }
+        }
+    }
+
+    0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_process_data() {
+        let input = "2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533";
+        assert_eq!(102, process_data(input));
+    }
+}
